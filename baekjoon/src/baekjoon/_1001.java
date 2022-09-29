a3a644dbf391ce4d2eaf3e5a9778fcfa9c66c7f3
jm-back <backshren20@naver.com>
2022-09-29 18:08:21 +0900

    9월 29일 백준

--- baekjoon/src/baekjoon/_1001.java
+++ baekjoon/src/baekjoon/_1001.java
@@ -2,16 +2,15 @@ package baekjoon;
 
 import java.util.Scanner;
 
-public class _1000 {
+public class _1001 {
 
 	public static void main(String[] args) {
-		
 		Scanner sc = new Scanner(System.in);
 		
 		int a = sc.nextInt();
 		int b = sc.nextInt();
 		
-		System.out.println(a+b);
+		System.out.println(a-b);
 		
 		
 		sc.close();
---
+++ baekjoon/src/baekjoon/_2557.java
@@ -0,0 +1,11 @@
+package baekjoon;
+
+public class _2557 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+
+	}
+
+}
