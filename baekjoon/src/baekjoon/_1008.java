90b871d8eacb61c564882efe8775ddd41059c742
jm-back <backshren20@naver.com>
2022-10-03 16:19:05 +0900

    10월 3일 백준

---
+++ baekjoon/src/baekjoon/_1008.java
@@ -0,0 +1,18 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class _1008 {
+	//나눗셈
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double num1 = sc.nextDouble();
+		double num2 = sc.nextDouble();
+		
+		System.out.println(num1 / num2);
+		
+
+		sc.close();
+	}
+
+}
