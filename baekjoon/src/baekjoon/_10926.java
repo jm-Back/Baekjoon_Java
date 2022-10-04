d18ebf118d32182a3221d7cfb9449538b875bdbe
jm-back <backshren20@naver.com>
2022-10-04 13:52:35 +0900

    10월 4일 백준

---
+++ baekjoon/src/baekjoon/_10926.java
@@ -0,0 +1,16 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class _10926 {
+	//사칙연산
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String id = sc.next();
+		
+		System.out.println(id + "??!");
+		
+		sc.close();
+	}
+
+}
