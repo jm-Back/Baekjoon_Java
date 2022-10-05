07345f6c4b74a69584821df3132be19366a3dcd6
jm-back <backshren20@naver.com>
2022-10-05 16:07:46 +0900

    10월 5일 백준

---
+++ baekjoon/src/baekjoon/_18108.java
@@ -0,0 +1,21 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class _18108 {
+
+	public static void main(String[] args) {
+		// 1998년생인데 태국에서 2541년생?
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int y = sc.nextInt();
+		
+		System.out.println(y-543);
+		
+		
+		sc.close();
+
+	}
+
+}
