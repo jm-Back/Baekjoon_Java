9ff3f9bb4ba73597824ecde9b9711c709a2cc227
jm-back <backshren20@naver.com>
2023-06-24 11:40:37 +0900

    5622번 다이얼 문제는 미해결이다... CASE 문을 안쓰고 문자+연산으로 풀었는데 어디가 틀렸는지 고민해야겠다.

---
+++ baekjoon/src/baekjoon/_11718.java
@@ -0,0 +1,23 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+ 
+/**
+ * 그대로 출력하기 
+ */
+public class _11718 {
+	
+	public static void main(String args[]) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); //선언
+		
+		String s;
+		
+		while ((s = br.readLine()) != null) {
+            System.out.println(s);
+        }
+		
+	}
+}
---
+++ baekjoon/src/baekjoon/_5622.java
@@ -0,0 +1,46 @@
+package baekjoon;
+
+import java.io.IOException;
+import java.util.Scanner;
+ 
+/**
+ * 다이얼
+ * 오래된 다이얼 전화기가 있다.
+ * 숫자를 누르고 돌려야한다. 
+ * 숫자를 누른 뒤에는 다시 처음으로 돌아가며, 숫자 1을 걸려면 총 2초가 필요하다. 
+ * 한 칸 옆에 있는 숫자를 걸기 위해서는 1초씩 더 걸린다.
+ * 할머니는 전화번호를 각 숫자에 해당하는 문자로 외운다.
+ * 할머니가 외운 단어가 주어졌을 때, 전화를 걸기 위해 필요한 최소 시간을 구하시오.
+ */
+public class _5622 {
+	
+	public static void main(String args[]) throws IOException{
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String n = sc.next();
+		
+		n = n.toUpperCase();
+		
+		int sum = 0;
+		
+		for ( int i = 0; i < n.length(); i++ )
+		{
+			char num = n.charAt(i);
+
+			if ( num == 83 || num == 90 )
+			{
+				sum += (num-65)/3 + 2;
+			}
+			else
+			{
+				sum += (num-65)/3 + 3;
+			}
+			
+		}
+
+		System.out.println(sum);
+		
+		sc.close();
+	}
+}
