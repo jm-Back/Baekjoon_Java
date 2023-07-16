2e9ffbae7c9332fbbe3686366a89aac1b327f6fc
jm-back <backshren20@naver.com>
2023-07-16 21:22:24 +0900

    알고리즘 수업 1

---
+++ baekjoon/src/baekjoon/_24262.java
@@ -0,0 +1,32 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+/**
+ * 
+ * 알고리즘 수업 - 알고리즘 수행 시간1
+ * 서준이는 알고리즘의 수행시간 수업 조교를 하고 있다.
+ * 
+ * 입력 크기 n이 주어지면 MenOfPassion 알고리즘 수행 시간을 다음과 같이 출력하자.
+ * MenOfPassion(A[], n) {
+    i = ⌊n / 2⌋;
+    return A[i]; # 코드1
+}
+ *
+ * 첫 째 줄에는 n 이 주어진다. 
+ * 
+ * 1. 수행 횟수를 출력한다. 
+ * 둘째 줄에는 코드1의 수행 횟수를 다항식으로 나타냈을 때, 최고차항의 차수를 출력한다. 
+ * 다만, 다항식으로 나타낼 수 없거나 최고차항의 차수가 3보다 크면 4를 출력한다. 
+ * 
+ */
+public class _24262 {
+	//나눗셈
+	public static void main(String[] args) {
+		
+		System.out.println(1);
+		System.out.println(0);
+		
+	}
+
+}
