e7ff18900304c9f21f741c8f10b9c1ed7e198891
jm-back <backshren20@naver.com>
2023-08-27 17:58:42 +0900

    드디어 풀었다!!! 백준 블랙잭~~!!

---
+++ baekjoon/src/baekjoon/_2798.java
@@ -0,0 +1,48 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _2798 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		int [] arr = new int[n];
+		
+		st = new StringTokenizer(br.readLine());
+		for ( int i = 0; i < n; i++ ) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int max = 0;
+		for ( int i = 0; i < n-2; i++ ) {
+			for ( int j = i+1; j < n-1; j++ ) {
+				for ( int k = j+1; k < n; k++ ) {
+					
+					int sum = arr[i] + arr[j] + arr[k];
+//					System.out.print(sum + " : 구성 = ");
+//					System.out.println(arr[i] +" "+ arr[j] +" "+ arr[k]);
+//					System.out.println();
+					
+					if ( sum <= m ) {
+						max = Math.max(max, sum);
+					}
+					
+					
+				}
+			}
+		}
+		
+		System.out.println(max);
+		
+		
+		
+	}
+}
